7928ced674f5601c31158590f0949d3c05ad35c7
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-04-10 10:06:35 -1100

    feat: Created, next app, clean up and set up necessary defaults.

---
+++ app/components/Logo.jsx
@@ -0,0 +1,253 @@
+import React from "react";
+
+export const Logo = () => {
+  return (
+    <a href="/">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="105"
+        height="44"
+        fill="none"
+        class="styles_logo__E0H0_"
+      >
+        <path
+          fill="url(#logo_svg__a)"
+          d="M48.537 35.96V16.475H52.1v7.878l7.21-7.878h4.4l-7.183 7.74 7.377 11.747h-4.37l-5.568-9.02-1.866 2.033v6.987h-3.563Zm17.723 0V15.918h3.564v20.044H66.26Zm11.965.335c-1.188 0-2.162-.186-2.923-.557-.76-.39-1.327-.9-1.698-1.53a4.042 4.042 0 0 1-.557-2.089c0-1.28.501-2.32 1.503-3.118 1.002-.798 2.506-1.197 4.51-1.197h3.508v-.334c0-.946-.27-1.642-.808-2.088-.538-.445-1.206-.668-2.004-.668-.724 0-1.355.177-1.893.53-.538.333-.872.834-1.002 1.503h-3.48c.093-1.003.427-1.875 1.002-2.617.594-.743 1.355-1.309 2.283-1.698.928-.409 1.967-.613 3.118-.613 1.967 0 3.517.492 4.649 1.476 1.132.983 1.698 2.375 1.698 4.175v8.49h-3.034l-.334-2.226a5.106 5.106 0 0 1-1.726 1.837c-.724.483-1.661.724-2.812.724Zm.807-2.784c1.021 0 1.81-.334 2.367-1.002.575-.668.937-1.494 1.085-2.478H79.45c-.947 0-1.624.177-2.032.53-.409.333-.613.75-.613 1.252 0 .538.204.956.613 1.253.408.297.946.445 1.614.445Zm16.206 2.784c-1.225 0-2.301-.195-3.229-.585-.928-.408-1.67-.965-2.227-1.67a4.795 4.795 0 0 1-1.002-2.45h3.591c.111.539.408 1.003.89 1.392.502.371 1.142.557 1.922.557s1.345-.158 1.698-.473c.371-.316.557-.678.557-1.086 0-.594-.26-.993-.78-1.197-.52-.223-1.243-.436-2.171-.64a23.715 23.715 0 0 1-1.81-.474 9.292 9.292 0 0 1-1.698-.695 3.934 3.934 0 0 1-1.225-1.114c-.315-.464-.473-1.03-.473-1.698 0-1.225.482-2.255 1.447-3.09.984-.835 2.358-1.253 4.12-1.253 1.634 0 2.933.38 3.898 1.142.984.76 1.568 1.809 1.754 3.145h-3.369c-.204-1.02-.974-1.53-2.31-1.53-.668 0-1.188.129-1.56.389-.352.26-.528.584-.528.974 0 .408.269.733.807.974.538.242 1.253.465 2.144.669.965.222 1.846.473 2.644.751.817.26 1.467.66 1.949 1.197.483.52.724 1.272.724 2.255a3.88 3.88 0 0 1-.668 2.31c-.464.687-1.132 1.226-2.005 1.615-.872.39-1.902.585-3.09.585Z"
+        ></path>
+        <g filter="url(#logo_svg__b)">
+          <ellipse
+            cx="20.849"
+            cy="25.42"
+            fill="#fff"
+            rx="18.849"
+            ry="16.564"
+          ></ellipse>
+        </g>
+        <g filter="url(#logo_svg__c)">
+          <ellipse
+            cx="20.849"
+            cy="20.278"
+            fill="#6C6C6C"
+            rx="18.849"
+            ry="18.278"
+          ></ellipse>
+        </g>
+        <ellipse
+          cx="20.849"
+          cy="19.706"
+          fill="#F7F7F7"
+          rx="18.849"
+          ry="17.706"
+        ></ellipse>
+        <ellipse
+          cx="20.848"
+          cy="19.136"
+          fill="url(#logo_svg__d)"
+          rx="15.422"
+          ry="14.851"
+        ></ellipse>
+        <circle cx="14.564" cy="19.138" r="3.427" fill="#fff"></circle>
+        <circle cx="27.13" cy="19.138" r="3.427" fill="#fff"></circle>
+        <defs>
+          <linearGradient
+            id="logo_svg__a"
+            x1="74.496"
+            x2="74.496"
+            y1="8.961"
+            y2="44.961"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#0473E8"></stop>
+            <stop offset="1" stop-color="#7A46FD"></stop>
+          </linearGradient>
+          <linearGradient
+            id="logo_svg__d"
+            x1="20.848"
+            x2="20.848"
+            y1="4.285"
+            y2="33.986"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#0374E8"></stop>
+            <stop offset="1" stop-color="#7A46FD"></stop>
+          </linearGradient>
+          <filter
+            id="logo_svg__b"
+            width="39.476"
+            height="34.906"
+            x="1.111"
+            y="8.855"
+            color-interpolation-filters="sRGB"
+            filterUnits="userSpaceOnUse"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+            <feColorMatrix
+              in="SourceAlpha"
+              result="hardAlpha"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            ></feColorMatrix>
+            <feOffset dy="0.888"></feOffset>
+            <feGaussianBlur stdDeviation="0.444"></feGaussianBlur>
+            <feComposite in2="hardAlpha" operator="out"></feComposite>
+            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"></feColorMatrix>
+            <feBlend
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_10671_11687"
+            ></feBlend>
+            <feBlend
+              in="SourceGraphic"
+              in2="effect1_dropShadow_10671_11687"
+              result="shape"
+            ></feBlend>
+          </filter>
+          <filter
+            id="logo_svg__c"
+            width="39.984"
+            height="38.839"
+            x="0.858"
+            y="0.858"
+            color-interpolation-filters="sRGB"
+            filterUnits="userSpaceOnUse"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+            <feBlend
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            ></feBlend>
+            <feGaussianBlur
+              result="effect1_foregroundBlur_10671_11687"
+              stdDeviation="0.571"
+            ></feGaussianBlur>
+          </filter>
+        </defs>
+      </svg>
+    </a>
+  );
+};
+
+export const SmallLogo = () => {
+  return (
+    <a href="/">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="94"
+        height="36"
+        fill="none"
+        class="styles_logoMobile__vfKM6"
+      >
+        <path
+          fill="url(#logo-m_svg__a)"
+          d="M41.735 27V8.414h3.398v7.514l6.877-7.514h4.196l-6.85 7.381L56.39 27h-4.168l-5.31-8.603-1.78 1.938V27h-3.398Zm16.904 0V7.882h3.399V27h-3.399Zm11.412.319c-1.133 0-2.062-.177-2.788-.531-.726-.372-1.266-.859-1.62-1.46a3.856 3.856 0 0 1-.53-1.992c0-1.222.477-2.213 1.433-2.974.956-.761 2.39-1.142 4.301-1.142h3.346v-.318c0-.903-.257-1.567-.77-1.992-.513-.425-1.15-.637-1.912-.637-.69 0-1.292.168-1.805.504-.514.32-.832.797-.956 1.434h-3.32c.09-.956.408-1.788.957-2.496a5.42 5.42 0 0 1 2.177-1.62c.885-.389 1.876-.584 2.974-.584 1.876 0 3.354.47 4.434 1.408s1.62 2.266 1.62 3.983V27h-2.894l-.32-2.124a4.87 4.87 0 0 1-1.645 1.752c-.69.46-1.585.69-2.682.69Zm.77-2.656c.974 0 1.726-.318 2.257-.956.549-.637.894-1.424 1.035-2.363H71.22c-.903 0-1.549.169-1.938.505-.39.319-.584.717-.584 1.195 0 .513.194.911.584 1.195.39.283.903.424 1.54.424Zm15.457 2.656c-1.168 0-2.195-.186-3.08-.558-.885-.39-1.593-.92-2.124-1.593a4.574 4.574 0 0 1-.956-2.337h3.425c.106.514.39.956.85 1.328.478.354 1.089.531 1.832.531.744 0 1.283-.15 1.62-.451.354-.301.53-.646.53-1.036 0-.566-.247-.947-.743-1.142-.495-.212-1.186-.416-2.07-.61a22.615 22.615 0 0 1-1.727-.452 8.862 8.862 0 0 1-1.62-.664 3.752 3.752 0 0 1-1.168-1.062c-.3-.442-.451-.982-.451-1.62 0-1.168.46-2.15 1.38-2.947.939-.796 2.249-1.194 3.93-1.194 1.558 0 2.797.362 3.718 1.088.938.726 1.496 1.726 1.672 3h-3.212c-.195-.973-.93-1.46-2.204-1.46-.637 0-1.133.124-1.487.372-.336.248-.505.558-.505.93 0 .389.257.698.77.929.514.23 1.195.442 2.045.637.92.212 1.761.451 2.522.717.78.247 1.399.628 1.86 1.141.46.496.69 1.213.69 2.151a3.698 3.698 0 0 1-.638 2.204c-.442.655-1.08 1.168-1.912 1.54-.831.372-1.814.558-2.947.558Z"
+        ></path>
+        <g filter="url(#logo-m_svg__b)">
+          <ellipse
+            cx="17.356"
+            cy="20.19"
+            fill="#fff"
+            rx="15.356"
+            ry="13.495"
+          ></ellipse>
+        </g>
+        <g filter="url(#logo-m_svg__c)">
+          <ellipse
+            cx="17.356"
+            cy="16.004"
+            fill="#6C6C6C"
+            rx="15.356"
+            ry="14.89"
+          ></ellipse>
+        </g>
+        <ellipse
+          cx="17.356"
+          cy="15.538"
+          fill="#F7F7F7"
+          rx="15.356"
+          ry="14.425"
+        ></ellipse>
+        <ellipse
+          cx="17.357"
+          cy="15.075"
+          fill="url(#logo-m_svg__d)"
+          rx="12.564"
+          ry="12.098"
+        ></ellipse>
+        <circle cx="12.237" cy="15.073" r="2.792" fill="#fff"></circle>
+        <circle cx="22.476" cy="15.073" r="2.792" fill="#fff"></circle>
+        <defs>
+          <linearGradient
+            id="logo-m_svg__a"
+            x1="66.494"
+            x2="66.494"
+            y1="0"
+            y2="29.609"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#0473E8"></stop>
+            <stop offset="1" stop-color="#7A46FD"></stop>
+          </linearGradient>
+          <linearGradient
+            id="logo-m_svg__d"
+            x1="17.357"
+            x2="17.357"
+            y1="2.977"
+            y2="27.174"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#0374E8"></stop>
+            <stop offset="1" stop-color="#7A46FD"></stop>
+          </linearGradient>
+          <filter
+            id="logo-m_svg__b"
+            width="32.406"
+            height="28.683"
+            x="1.153"
+            y="6.695"
+            color-interpolation-filters="sRGB"
+            filterUnits="userSpaceOnUse"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+            <feColorMatrix
+              in="SourceAlpha"
+              result="hardAlpha"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            ></feColorMatrix>
+            <feOffset dy="0.847"></feOffset>
+            <feGaussianBlur stdDeviation="0.424"></feGaussianBlur>
+            <feComposite in2="hardAlpha" operator="out"></feComposite>
+            <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"></feColorMatrix>
+            <feBlend
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_10671_13701"
+            ></feBlend>
+            <feBlend
+              in="SourceGraphic"
+              in2="effect1_dropShadow_10671_13701"
+              result="shape"
+            ></feBlend>
+          </filter>
+          <filter
+            id="logo-m_svg__c"
+            width="32.89"
+            height="31.96"
+            x="0.91"
+            y="0.024"
+            color-interpolation-filters="sRGB"
+            filterUnits="userSpaceOnUse"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+            <feBlend
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            ></feBlend>
+            <feGaussianBlur
+              result="effect1_foregroundBlur_10671_13701"
+              stdDeviation="0.545"
+            ></feGaussianBlur>
+          </filter>
+        </defs>
+      </svg>
+    </a>
+  );
+};
---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Logo } from "./components/Logo";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+};
+
+export default HomePage;
