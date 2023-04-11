692febbee5274d2ee84fa5df19a26f6ffeb21dfa
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-04-10 14:31:32 -1100

    feat: added font - DMsans

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <div className="container">
       <Logo />
-      <h2 className="text-purple-100 p-3 underline text-2xl bg-green-200">hello</h2>
+      <h2 className="text-purple-100 p-3 text-2xl">hello My name is david - testing 123</h2>
     </div>
   );
 };
